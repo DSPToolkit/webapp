f92834663570ba741a28470f6c91d263f7abd01d
Iman Alavi Fazel <im.alavifazel@gmail.com>
2025-12-02 21:22:36 +0330

    Fix the help message of the interactive prompt

--- src/app/components/AppFrame/Prompt/Prompt.tsx
+++ src/app/components/AppFrame/Prompt/Prompt.tsx
@@ -10,7 +10,7 @@ export const Prompt = () => {
 
   return (
 
-    <div className="w-full flex flex-col mb-5 shadow rounded ">
+    <div className="w-full flex flex-col mb-5 shadow rounded">
       <div className="bg-white flex-1 p-5 text-slate-600 overflow-y-auto w-full">
         <div className="flex flex-col">
           {
@@ -24,10 +24,8 @@ export const Prompt = () => {
 
       <div className="flex">
         <input value={cmd} onKeyDown={e => e.key === 'Enter' ? handleButton() : ''} onChange={(event) => setCmd(() => event.target.value)} className="w-full rounded shadow-lg p-1 border-2 border-gray-100" placeholder="Enter 'help' to get available commands..." type="text" />
-        <button onClick={handleButton}
-                
-                className="rounded-lg border border-slate-300 hover:bg-gray-200 py-2 px-5">Enter</button>
-
+        <button onClick={handleButton}      
+          className="rounded-lg border border-slate-300 hover:bg-gray-200 py-2 px-5">Enter</button>
       </div>
 
     </div>
